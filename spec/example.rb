c813aa09112979d52d829025c6015779041c7bff
pkuykendall <pkuykendall@omegafi.com>
2015-01-12 22:47:26 -0500

    Added tests for assignments from an 'if' statement or block.

--- spec/example.rb
+++ spec/example.rb
@@ -92,3 +92,21 @@ puts "This is multi line interpolated string #{
 x
 }"
 end
+
+# Test for variable assignment from an if statement
+@products = if params[:category]
+Category.find(params[:category]).products
+else
+Product.all
+end
+
+# Test for variable assignment from a block
+response = begin
+if true?
+api_call(test)
+else
+rejected
+end
+rescue
+'FALSE'
+end
--- spec/example_beautified.rb
+++ spec/example_beautified.rb
@@ -92,3 +92,21 @@ def m (x)
 		x
 	}"
 end
+
+# Test for variable assignment from an if statement
+@products = if params[:category]
+	Category.find(params[:category]).products
+else
+	Product.all
+end
+
+# Test for variable assignment from a block
+response = begin
+	if true?
+		api_call(test)
+	else
+		rejected
+	end
+rescue
+	'FALSE'
+end
