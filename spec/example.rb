49da44d7e72dc3176c9565daada081a9e9a022be
pkuykendall <pkuykendall@omegafi.com>
2015-01-12 22:53:07 -0500

    Added tests for formatting case statements.

--- spec/example.rb
+++ spec/example.rb
@@ -92,3 +92,15 @@ puts "This is multi line interpolated string #{
 x
 }"
 end
+
+# Test to validate case statements
+def case_test opts=nil
+case test_case
+when 'Passing'
+call(:pass)
+when 'Failing'
+call(:fail)
+when 'Error'
+call(:error)
+end
+end
--- spec/example_beautified.rb
+++ spec/example_beautified.rb
@@ -92,3 +92,15 @@ def m (x)
 		x
 	}"
 end
+
+# Test to validate case statements
+def case_test opts=nil
+	case test_case
+	when 'Passing'
+		call(:pass)
+	when 'Failing'
+		call(:fail)
+	when 'Error'
+		call(:error)
+	end
+end
