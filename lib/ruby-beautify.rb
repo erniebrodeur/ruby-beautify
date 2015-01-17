54f3ee9abc792bbd0c0644e6e2e6c74361d511db
Ernie Brodeur <ebrodeur@ujami.net>
2015-01-17 15:06:25 -0800

    Converted to tabs.

--- lib/ruby-beautify.rb
+++ lib/ruby-beautify.rb
@@ -2,10 +2,10 @@ require 'open3'
 require 'ruby-beautify/version'
 
 module RubyBeautify
-  extend self
-  def syntax_ok?(string)
-    out, err, status = Open3.capture3("ruby -c -", stdin_data:string )
-    return false unless err.empty?
-    return true
-  end
+	extend self
+	def syntax_ok?(string)
+		out, err, status = Open3.capture3("ruby -c -", stdin_data:string )
+		return false unless err.empty?
+		return true
+	end
 end
