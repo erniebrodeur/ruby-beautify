1e24b822a12c8a7e3f5566d8620f3dbae63f7f6c
Ernie Brodeur <ebrodeur@ujami.net>
2015-01-17 19:19:06 -0800

    tmp isn't built on travis (or committed).

--- spec/bin/ruby-beautify_spec.rb
+++ spec/bin/ruby-beautify_spec.rb
@@ -9,9 +9,10 @@ describe "Ruby Beautify" do
 		@doubled_file = 'spec/binary_scenarios/doubled_example.rb'
 		# Our file to overwrite (should be ugly to start).
 		@overwrite_file = "spec/binary_scenarios/overwrite.rb"
-		@overwrite_target_file = "#{Dir.pwd}/tmp/copied"
+		@overwrite_target_file = "tmp/copied.rb"
 		@overwrite_pretty_file = "spec/binary_scenarios/overwrite_pretty.rb"
 	end
+
 	it "will work" do
 		small_md5_sum = Digest::MD5.hexdigest File.read(@small_file)
 		md5_sum = Digest::MD5.hexdigest `bundle exec #{BEAUTIFY_BIN} #{@small_file}`
@@ -26,11 +27,13 @@ describe "Ruby Beautify" do
 
 	# I want to make sure the file actually changes, so I do this (I could make yet another file).
 	it "will update files (overwrite) in place" do
+		FileUtils.mkdir_p File.dirname @overwrite_target_file
 		FileUtils.cp @overwrite_file, @overwrite_target_file
 		`bundle exec #{BEAUTIFY_BIN} --overwrite #{@overwrite_target_file}`
 		md5_sum = Digest::MD5.hexdigest File.read @overwrite_target_file
 	 	overwrite_md5_sum = Digest::MD5.hexdigest File.read(@overwrite_pretty_file)
 		expect(md5_sum).to eq overwrite_md5_sum
+		FileUtils.rm @overwrite_target_file
 	end
 
 	it "will honor --tabs vs --spaces"
